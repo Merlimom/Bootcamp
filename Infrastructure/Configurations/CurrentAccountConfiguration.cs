268af54beb9779271ddd180a3866d9ba82e1c1fc
. <.>
2024-04-11 14:52:27 -0400

    Account configuration

--- Infrastructure/Configurations/CurrentAccountConfiguration.cs
+++ Infrastructure/Configurations/CurrentAccountConfiguration.cs
@@ -27,8 +27,8 @@ public class CurrentAccountConfiguration : IEntityTypeConfiguration<CurrentAccou
 
 
         entity.HasOne(d => d.Account)
-              .WithMany(p => p.CurrentAccounts)
-              .HasForeignKey(d => d.AccountId);
-        
+              .WithOne(p => p.CurrentAccount)
+              .HasForeignKey<CurrentAccount>(d => d.AccountId);
+
     }
 }
